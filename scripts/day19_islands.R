50fb6dd0d4feea87628fed5997a76367a5657cf0
ivabrunec <iva.brunec@gmail.com>
2021-11-19 09:50:28 -0500

    day 19 updated

--- scripts/day19_islands.R
+++ scripts/day19_islands.R
@@ -22,10 +22,10 @@ elmat <- raster_to_matrix(elev)
 #aed9e0
 #b2d6dc
 elmat %>%
-  sphere_shade(texture = create_texture('#F27A7A', '#000000', '#7A472E', '#1A6408', '#B9D4E5')) %>%
+  sphere_shade(texture = create_texture('#F27A7A', '#000000', '#7A472E', '#1A6408', '#aed9e0')) %>%
   #add_shadow(ray_shade(elmat, sunaltitude=20, sunangle = 90)) %>%
   #add_shadow(ambient_shade(elmat)) %>%
   #add_water(detect_water(elmat),color="imhof4") %>% # this works but i didn't like how it looked
-  plot_3d(elmat, zscale = 2, theta = 0, phi=60, zoom = .56, windowsize = c(1200, 800), background = '#B9D4E5', solid = F, shadow = F)
+  plot_3d(elmat, zscale = 2, theta = 0, phi=60, zoom = .56, windowsize = c(1200, 800), background = '#aed9e0', solid = F, shadow = F)
 
-render_snapshot('day19_islands', clear = F)
+render_snapshot('day19_islands2', clear = F)
