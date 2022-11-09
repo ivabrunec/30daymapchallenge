5c055c016243a433f6d0a88ddc2d8b4353ead679
ivabrunec <30981504+ivabrunec@users.noreply.github.com>
2022-11-09 18:32:16 -0500

    added day 9

---
+++ scripts/2022/day9_space.R
@@ -0,0 +1,43 @@
+## Day 9: Space.
+
+# raster of the moon
+# moon data from https://svs.gsfc.nasa.gov/cgi-bin/details.cgi?aid=4720
+
+setwd(dirname(rstudioapi::getSourceEditorContext()$path)) 
+library(rayrender)
+library(dplyr)
+
+# background
+# from https://www.pexels.com/photo/milky-way-galaxy-during-nighttime-1252890/
+
+# testing rayrender
+scene = 
+  add_object(sphere(y=0.2,material=glossy(image_texture = 'moon.png',reflectance=0.05),angle=c(0,-90,0))) |>
+  add_object(sphere(y=1.5, x = 1.5, radius = .25, 
+                    material=glossy(image_texture = '2kearth.png',reflectance=0.05),angle=c(0,-90,0))) 
+
+# static/interactive image
+render_scene(scene, parallel = TRUE, width = 800, 
+             ambient_light = TRUE,
+             height = 800, samples = 800,
+             environment_light = 'free_nature.png',
+             intensity_env = 3,
+             filename = 'moon_earth4.png')
+
+
+# short video: render times are a bit silly so i abandoned this, but it would work
+frames = 360
+
+camerax=-25*cos(seq(0,360,length.out = frames+1)[-frames-1]*pi/180)
+cameraz=25*sin(seq(0,360,length.out = frames+1)[-frames-1]*pi/180)
+
+for(i in 1:frames) {
+  render_scene(scene, width=600, height=600, fov=35,
+               lookfrom = c(camerax[i],25,cameraz[i]),
+               lookat = c(0,9,0), samples = 300, parallel = TRUE,
+               filename=glue::glue("earth_moon{i}"))
+}
+
+av::av_encode_video(glue::glue("earth_moon{1:(frames-1)}.png"), framerate=60, output = "earth_moon.mp4")
+file.remove(glue::glue("earth_moon{1:(frames-1)}.png"))
+ 
