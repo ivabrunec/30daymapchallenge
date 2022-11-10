0c4dafc185a733473bd9ea7fb49e89243e83ee78
ivabrunec <iva.brunec@gmail.com>
2022-11-10 09:10:05 -0500

    updated day 10

--- scripts/2022/day10_badmap.R
+++ scripts/2022/day10_badmap.R
@@ -26,11 +26,11 @@ ggplot()+
   geom_sf(data = aust_geom, fill = 'grey60', color = '#bcabae') +
   geom_point(data = bad_data, aes(x = V6, y = V5 - .05)) +
   geom_text(data = bad_data, aes(x = V6, y = V5+.02, label = V2),
-            size = 6, color = '#2d2e2e', family = 'Roboto Condensed') +
+            size = 8, color = '#2d2e2e', family = 'Roboto Condensed') +
   theme_void() +
   theme(plot.background = element_rect(fill = '#DAC8CB', color = NA),
         plot.title = element_text(hjust = .5, color = '#2d2e2e',
-                                  family = 'Roboto Condensed', size = 20)) +
+                                  family = 'Roboto Condensed', size = 50)) +
   labs(title = 'This is a Bad map')
 
-ggsave('day10_badmap.png', width = 6, height = 3, dpi = 300)
+ggsave('day10_badmap.png', width = 6, height = 4, dpi = 300)
